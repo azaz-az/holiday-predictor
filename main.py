6d626b329244ccb5970e3bdb1a1238c7cbfa94d3
azaz-az <a233d@outlook.com>
2024-05-01 09:02:41 +0800

    feat: 预测周一的五一假期的功能

--- main.py
+++ main.py
@@ -17,12 +17,11 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 __all__ = ["calculation"]
 
-from types import ModuleType
 from typing import List, Optional
 
 import datetime
 
-from zhdate import ZhDate
+from zhdate import ZhDate # type: ignore
 
 from data import Data
 
@@ -42,7 +41,7 @@ def calculation(given_list: List[str]) -> str:
     # 下述代码从传入参数中获取必要信息。
     forecast_year: int = int(given_list[1])
     holiday_name: str = str(given_list[2])
-    hld_startdate: Optional[datetime.datetime] = None
+    hld_startdate: Optional[datetime.datetime] = None # type: ignore
     hld_enddate: Optional[datetime.datetime] = None
     hld_days: int
 
@@ -55,7 +54,7 @@ def calculation(given_list: List[str]) -> str:
             return ("错误的输入。给定年份 {year} "
                     "不存在国庆假期。".format(year=forecast_year))
         # 下述代码对中秋节、国庆节的日期进行运算。
-        mid_date: ZhDate = ZhDate(
+        mid_date: datetime.datetime = ZhDate(
             forecast_year, 8, 15
         ).to_datetime()  # 该变量是中秋节的日期。
         nati_date = datetime.datetime(
@@ -223,7 +222,7 @@ def calculation(given_list: List[str]) -> str:
             raise AssertionError
 
     elif holiday_name in ("--duan-wu", "-dw"):  # 该部分用于处理端午假期的调休预测。
-        duan_wu_date: ZhDate = ZhDate(forecast_year, 5, 5).to_datetime()
+        duan_wu_date: datetime.datetime = ZhDate(forecast_year, 5, 5).to_datetime()
         duan_wu_dateofweek: int = duan_wu_date.weekday()
         # 下述代码对端午假期的调休进行运算。
         if duan_wu_dateofweek == 0:
@@ -258,6 +257,45 @@ def calculation(given_list: List[str]) -> str:
             hld_days = 3
             hld_startdate = duan_wu_date - datetime.timedelta(days=1)
             hld_enddate = duan_wu_date + datetime.timedelta(days=1)
+
+    elif holiday_name in ("--may-day", "-md"):  # 该部分用于处理五一假期的调休预测。
+        may_day_date: datetime.datetime = datetime.datetime(year=forecast_year, month=5, day=1)
+        may_day_dateofweek: int = may_day_date.weekday()
+        # 下述代码对五一假期的调休进行运算。
+        if may_day_dateofweek == 0:
+            # 调休
+            hld_days = 5
+            hld_startdate = may_day_date - datetime.timedelta(days=2)
+            hld_enddate = may_day_date + datetime.timedelta(days=2)
+            lieu_1 = may_day_date - datetime.timedelta(days=8)
+            lieu_2 = may_day_date + datetime.timedelta(days=5)
+        elif may_day_dateofweek == 1:
+            # 调休
+            hld_days = 3
+            hld_startdate = may_day_date - datetime.timedelta(days=2)
+            hld_enddate = may_day_date
+            lieu_1 = may_day_date - datetime.timedelta(days=3)
+        elif may_day_dateofweek == 2:
+            # 不调休
+            hld_days = 1
+            hld_startdate = may_day_date
+            hld_enddate = may_day_date
+        elif may_day_dateofweek == 3:
+            # 调休
+            hld_days = 3
+            hld_startdate = may_day_date
+            hld_enddate = may_day_date + datetime.timedelta(days=2)
+            lieu_1 = may_day_date + datetime.timedelta(days=3)
+        elif may_day_dateofweek in (4, 5):
+            # 不调休
+            hld_days = 3
+            hld_startdate = may_day_date
+            hld_enddate = may_day_date + datetime.timedelta(days=2)
+        elif may_day_dateofweek == 6:
+            # 不调休
+            hld_days = 3
+            hld_startdate = may_day_date - datetime.timedelta(days=1)
+            hld_enddate = may_day_date + datetime.timedelta(days=1)
         else:
             raise AssertionError
 
