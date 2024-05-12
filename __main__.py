6a164b39fda2c4b67bf8a67a07e242c3aa2be2e4
azaz-az <a233d@outlook.com>
2024-05-12 20:37:38 +0800

    :building_construction: 在文件行头加入注释

--- __main__.py
+++ __main__.py
@@ -1,3 +1,23 @@
+"""这是一个本项目的命令行 Demo 程序。
+"""
+
+"""
+Holiday Predictor / 假期预测器 - 基于 Python 的调休预测工具。
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from main import *
 
 import sys
